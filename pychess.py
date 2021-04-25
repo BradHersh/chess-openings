01e45108e81a2b6a89598f6bba324ea65eb2918d
DeanKezurer <dkezzy6@gmail.com>
2021-04-25 00:37:04 -0700

    Using the chess library

--- pychess.py
+++ pychess.py
@@ -1,3 +1,199 @@
-print('hello world')
+#%%
+import chess
+import numpy as np
 
+#%%
+board=chess.Board()
+board
+
+class1 = board.shredder_fen()
+end = class1.index(' ')
+
+#dir(board) -- to see attributes
+#%%
+#Capital letters = white pieces
+
+boardlayout = class1[:end]
+boardrows = boardlayout.split('/')
+boardrows = np.array([i for i in boardrows])
+collist = ['a','b','c','d','e','f','g','h']
+
+
+row_dict = {}
+
+for i in range(8):
+    row_dict[i+1] = boardrows[i]
+
+
+
+
+
+# %%
+board.turn == chess.BLACK
+board.piece_at(chess.D1)
+
+list(board.legal_moves)
+
+# %%
+import random
+
+move = list(board.legal_moves)[-1]
+def random_player(board):
+    move = random.choice(list(board.legal_moves))
+    return move.uci()
+
+for i in range(10):
+    print(random_player(board))
+
+
+# %%
+#Playing a game
+import time
+from IPython.display import display, HTML, clear_output
+
+def who(player):
+    return "White" if player == chess.WHITE else "Black"
+
+def display_board(board, use_svg):
+    if use_svg:
+        return board._repr_svg_()
+    else:
+        return "<pre>" + str(board) + "</pre>"
+
+def play_game(player1, player2, visual="svg", pause=0.1):
+    """
+    playerN1, player2: functions that takes board, return uci move
+    visual: "simple" | "svg" | None
+    """
+    use_svg = (visual == "svg")
+    board = chess.Board()
+    try:
+        while not board.is_game_over(claim_draw=True):
+            if board.turn == chess.WHITE:
+                uci = player1(board)
+            else:
+                uci = player2(board)
+            name = who(board.turn)
+            board.push_uci(uci)
+            board_stop = display_board(board, use_svg)
+            html = "<b>Move %s %s, Play '%s':</b><br/>%s" % (
+                       len(board.move_stack), name, uci, board_stop)
+            if visual is not None:
+                if visual == "svg":
+                    clear_output(wait=True)
+                display(HTML(html))
+                if visual == "svg":
+                    time.sleep(pause)
+    except KeyboardInterrupt:
+        msg = "Game interrupted!"
+        return (None, msg, board)
+    result = None
+    if board.is_checkmate():
+        msg = "checkmate: " + who(not board.turn) + " wins!"
+        result = not board.turn
+    elif board.is_stalemate():
+        msg = "draw: stalemate"
+    elif board.is_fivefold_repetition():
+        msg = "draw: 5-fold repetition"
+    elif board.is_insufficient_material():
+        msg = "draw: insufficient material"
+    elif board.can_claim_draw():
+        msg = "draw: claim"
+    if visual is not None:
+        print(msg)
+    return (result, msg, board)
+# %%
+play_game(random_player, random_player)
+# %%
+def human_player(board):
+    display(board)
+    uci = get_move("%s's move [q to quit]> " % who(board.turn))
+    legal_uci_moves = [move.uci() for move in board.legal_moves]
+    while uci not in legal_uci_moves:
+        print("Legal moves: " + (",".join(sorted(legal_uci_moves))))
+        uci = get_move("%s's move[q to quit]> " % who(board.turn))
+    return uci
+
+def get_move(prompt):
+    uci = input(prompt)
+    if uci and uci[0] == "q":
+        raise KeyboardInterrupt()
+    try:
+        chess.Move.from_uci(uci)
+    except:
+        uci = None
+    return uci
+
+
+#%%
+#intro statistical analysis
+
+def staticAnalysis(board, move, my_color):
+    score = 0
+    ## Check some things about this move:
+    # score += 10 if board.is_capture(move) else 0
+    # To actually make the move:
+    board.push(move)
+    # Now check some other things:
+    for (piece, value) in [(chess.PAWN, 1), 
+                           (chess.BISHOP, 4), 
+                           (chess.KING, 0), 
+                           (chess.QUEEN, 10), 
+                           (chess.KNIGHT, 5),
+                           (chess.ROOK, 3)]:
+        score += len(board.pieces(piece, my_color)) * value
+        score -= len(board.pieces(piece, not my_color)) * value
+        # can also check things about the pieces position here
+    return score
+
+def player1(board):
+    moves = list(board.legal_moves)
+    for move in moves:
+        newboard = board.copy()
+        # go through board and return a score
+        move.score = staticAnalysis(newboard, move, board.turn)
+    moves.sort(key=lambda move: move.score, reverse=True) # sort on score
+    return moves[0].uci()
+
+
+#%%
+play_game(player1, random_player)
+# %%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+########################################################################
+'''
+Plan:
+
+1) Generate board
+
+board = chess.Board()
+
+2) Use indexing of board spaces to create click / select variables
+
+
+
+3) Start the game but let humans click a square and it shows you what 
+moves you can do with that piece
+4) Play out the game
+'''
+#########################################################################
+
+#%%
 
