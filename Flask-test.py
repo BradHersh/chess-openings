8437b466735d21228ca19f3f5625fceb4f6d9fbf
DeanKezurer <dkezzy6@gmail.com>
2021-04-25 04:42:15 -0700

    Chess library board installation

--- Flask-test.py
+++ Flask-test.py
@@ -1,9 +1,25 @@
-from flask import Flask 
-app = Flask(__name__)
-@app.route("/")
+import chess
+import random
+import time
+from IPython.display import display, HTML, clear_output
 
-def hello():
-    return "Hello world!"
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__, template_folder = 'templates')
+
+def display_board(board):
+    return "<pre>" + str(board) + "</pre>"
+
+
+def execute():
+    board = chess.Board()
+    return display_board(board)
+
+
+@app.route('/')
+def home ():
+    return render_template('index.html', x= execute())
+
+
 
 if __name__ == "__main__":
     app.run()
