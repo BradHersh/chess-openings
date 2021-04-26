3fb7d8640b6e743452d8e581e45980791056cde5
brad_hershowitz <brad.hersh123@gmail.com>
2021-04-26 15:41:20 +0800

    created Brad-Hersh branch

---
+++ app/templates/openings/bogo-indian.js
@@ -0,0 +1,7 @@
+var moves = ['rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR',
+             'rnbqkb1r/pppppppp/5n2/8/3P4/8/PPP1PPPP/RNBQKBNR',
+             'rnbqkb1r/pppppppp/5n2/8/2PP4/8/PP2PPPP/RNBQKBNR',
+             'rnbqkb1r/pppp1ppp/4pn2/8/2PP4/8/PP2PPPP/RNBQKBNR',
+             'rnbqkb1r/pppp1ppp/4pn2/8/2PP4/5N2/PP2PPPP/RNBQKB1R',
+             'rnbqk2r/pppp1ppp/4pn2/8/1bPP4/5N2/PP2PPPP/RNBQKB1R'
+            ]
